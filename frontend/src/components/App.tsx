3a9141390806075203ccd9d88f6d99b39545ced1
sofibrezden <sofiabrezden9@gmail.com>
2025-07-07 23:23:26 +0300

    added Router

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,12 +1,22 @@
-import MainLayout from "./layouts/MainLayout";
-import HomeView from "./views/HomeView/HomeView";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomeView from './views/HomeView/HomeView';
+import HistoryView from './views/HistoryView/HistoryView';
+import {StyledEngineProvider} from '@mui/material/styles';
 
 function App() {
-  return (
-    <MainLayout>
-      <HomeView />
-    </MainLayout>
-  );
+    return (
+        <StyledEngineProvider injectFirst>
+            <Router>
+                <MainLayout>
+                    <Routes>
+                        <Route path="/" element={<HomeView/>}/>
+                        <Route path="/about-museum" element={<HistoryView/>}/>
+                    </Routes>
+                </MainLayout>
+            </Router>
+        </StyledEngineProvider>
+    );
 }
 
 export default App;
