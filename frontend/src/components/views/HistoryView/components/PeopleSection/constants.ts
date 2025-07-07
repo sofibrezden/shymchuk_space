b251b6ecd1015f188b242b15eaf5775b7c423e7d
sofibrezden <sofiabrezden9@gmail.com>
2025-07-07 23:27:33 +0300

    added famous people constants

---
+++ frontend/src/components/views/HistoryView/components/PeopleSection/constants.ts
@@ -0,0 +1,53 @@
+import Yushchenko from "../../../../../assets/images/history_page/yushchenko.png";
+import Klychko from "../../../../../assets/images/history_page/klychko.png";
+import Vlasuk from "../../../../../assets/images/history_page/vlasuk.png";
+import Prytula from "../../../../../assets/images/history_page/prytula.png";
+
+export const itemList = [
+    {
+        image: Yushchenko,
+        title: "Віктор Ющенко",
+        subtitle: "3-й Президент України,\nдержавний діяч"
+    },
+
+    {
+        image: Vlasuk,
+        title: "Олександр Власюк",
+        subtitle: "Казкар-лірник, Сашко Лірник"
+    },
+
+    {
+        image: Klychko,
+        title: "Віталій Кличко",
+        subtitle: "Український політик, мер столиці"
+    },
+
+    {
+        image: Prytula,
+        title: "Сергій Притула",
+        subtitle: "Волонтер, громадський та політичний діяч"
+    },
+    {
+        image: Yushchenko,
+        title: "Віктор Ющенко",
+        subtitle: "3-й Президент України,\nдержавний діяч"
+    },
+
+    {
+        image: Vlasuk,
+        title: "Олександр Власюк",
+        subtitle: "Казкар-лірник, Сашко Лірник"
+    },
+
+    {
+        image: Klychko,
+        title: "Віталій Кличко",
+        subtitle: "Український політик, мер столиці"
+    },
+
+    {
+        image: Prytula,
+        title: "Сергій Притула",
+        subtitle: "Волонтер, громадський та політичний діяч"
+    },
+];
