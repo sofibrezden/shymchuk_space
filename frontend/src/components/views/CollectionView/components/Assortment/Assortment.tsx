b418d855a7dfcfb00408230730864ced8792bc2d
sofibrezden <sofiabrezden9@gmail.com>
2025-07-09 19:59:40 +0300

    fix link

--- frontend/src/components/views/CollectionView/components/Assortment/Assortment.tsx
+++ frontend/src/components/views/CollectionView/components/Assortment/Assortment.tsx
@@ -26,7 +26,7 @@ function Assortment({
 
   return (
     <div className={styles.wrapper}>
-      <h5>Головна {">"} Колекція</h5>
+      <h5><a href="https://localhost:3000">Головна</a>{">"} Колекція</h5>
 
       <div className={styles.search}>
         <button
@@ -38,12 +38,12 @@ function Assortment({
 
         <input type="text" placeholder="Шукати" />
         <img src={SearchImg} alt="Search" className={styles.searchImg} />
-        <button
-          className={styles.mapButton}
-          onClick={() => setIsMapShown(!isMapShown)}
-        >
-          <img src={MapImg} className={styles.map} alt="Map" />
-        </button>
+        {/*<button*/}
+        {/*  className={styles.mapButton}*/}
+        {/*  onClick={() => setIsMapShown(!isMapShown)}*/}
+        {/*>*/}
+        {/*  <img src={MapImg} className={styles.map} alt="Map" />*/}
+        {/*</button>*/}
       </div>
 
       {isMapShown == true ? (
