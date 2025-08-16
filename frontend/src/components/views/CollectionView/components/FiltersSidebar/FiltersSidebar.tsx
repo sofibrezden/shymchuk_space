3ae9c1a5daf19d4b8a296b0de8e0d9e89c01a689
sofibrezden <sofiabrezden9@gmail.com>
2025-08-17 00:52:50 +0300

    added urls for regions pages

--- frontend/src/components/views/CollectionView/components/FiltersSidebar/FiltersSidebar.tsx
+++ frontend/src/components/views/CollectionView/components/FiltersSidebar/FiltersSidebar.tsx
@@ -1,18 +1,12 @@
-// FiltersSidebar.jsx
 import styles from "./FiltersSidebar.module.scss";
 import FilterImg from "../../../../../assets/images/collection_page/filter.png";
 import DownArrowImg from "../../../../../assets/icons/down_arrow_marker.svg";
 import RedDownArrowImg from "../../../../../assets/icons/red_down_arrow_marker.svg";
-import CloseImg from "../../../../../assets/icons/close.svg"; // Іконка закриття
-import { list } from "./constants";
+import CloseImg from "../../../../../assets/icons/close.svg";
+import { list, regionKeyMap } from "./constants";
 
-export default function FiltersSidebar({
-  isOpen,
-  isOpenHandler,
-}: {
-  isOpen: boolean;
-  isOpenHandler: (val: boolean) => void;
-}) {
+export default function FiltersSidebar({isOpen, isOpenHandler,}: {
+  isOpen: boolean; isOpenHandler: (val: boolean) => void; }) {
   return (
     <div className={`${styles.wrapper} ${isOpen ? styles.open : ""}`}>
       <div className={styles.backdrop} onClick={() => isOpenHandler(false)} />
@@ -40,32 +34,39 @@ export default function FiltersSidebar({
               />
               {item.title}
             </summary>
+
             <div className={styles.subsection}>
-              {item.subCategory?.map((sub) => (
-                <details key={sub.subCategoryName}>
-                  <summary>
-                    <img
-                      src={DownArrowImg}
-                      alt="Arrow"
-                      className={styles.arrowIcon}
-                    />
-                    {sub.subCategoryName}
-                  </summary>
-                  <div>
-                    <h6>
-                      Одяг <span>({sub.clothesCounter})</span>
-                    </h6>
-                    <ul>
-                      {sub.clothesList?.map((c) => (
-                        <li key={c}>• {c}</li>
-                      ))}
-                    </ul>
-                    <h6>
-                      Аксесуари <span>({sub.accessoriesCounter})</span>
-                    </h6>
-                  </div>
-                </details>
-              ))}
+              {item.subCategory?.map((sub) => {
+                const regionKey = regionKeyMap[sub.subCategoryName];
+
+                return (
+                  <details key={sub.subCategoryName}>
+                    <summary>
+                      <img
+                        src={DownArrowImg}
+                        alt="Arrow"
+                        className={styles.arrowIcon}
+                      />
+                      <a href={regionKey}>{sub.subCategoryName}</a>
+                    </summary>
+
+                    <div>
+                      <h6>
+                        Одяг <span>({sub.clothesCounter})</span>
+                      </h6>
+                      <ul>
+                        {sub.clothesList?.map((c) => (
+                          <li key={c}>• {c}</li>
+                        ))}
+                      </ul>
+
+                      <h6>
+                        Аксесуари <span>({sub.accessoriesCounter})</span>
+                      </h6>
+                    </div>
+                  </details>
+                );
+              })}
             </div>
           </details>
         ))}
